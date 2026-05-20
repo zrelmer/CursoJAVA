1846483c5938316e205b143ba6af2bda6bbd08ac
zrelmer <yattenelmer@gmail.com>
2026-05-20 16:32:16 -0600

    actualizado20-05-26

--- JAVA_APPS/10.OperadoresIncremento/Incremento.java
+++ JAVA_APPS/10.OperadoresIncremento/Incremento.java
@@ -26,21 +26,25 @@ public class Incremento {
 
         // ejemplo de pre-incremento en un ciclo for
         // el valor de i se muestra después de ser incrementado
-        for (int i = 1; i <= 5; ++i) {
-            System.out.println(1);
-            System.out.println(2);
-            System.out.println(3);
-            System.out.println(4);
-            System.out.println(5);
-        }
+        // for (int i = 1; i <= 5; ++i) {
+        //     System.out.println(1);
+        //     System.out.println(2);
+        //     System.out.println(3);
+        //     System.out.println(4);
+        //     System.out.println(5);
+        // }
 
-        // int a = 5;
-        // int b = 5;
+        int a = 5;
+        int b = 5;
 
-        // System.out.println(a++); // Imprime 5 (Aumenta a 6 después de imprimir)
-        // System.out.println(a);   // Imprime 6 (Valor actual de a)
-        // System.out.println(++b); // Imprime 6 (Aumenta a 6 antes de imprimir)
-
+        System.out.println(a++); // Imprime 5 (Aumenta a 6 después de imprimir)
+        System.out.println(a);  // Imprime 6 (Valor actual de a)
+        // System.out.println(a++);
+        // System.out.println(a);
+        System.out.println(++b); // Imprime 6 (Aumenta a 6 antes de imprimir)
+        // for (int i = 1; i <=10; i++) {
+        //     System.out.println(i + " Hola");
+        // }
     }
     
 }
